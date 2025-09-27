77dab9958e7b2bc0aa1cae9e2e1c62cc3059e088
User <harold.francois@ynov.com>
2025-09-27 14:45:08 +0200

    Ajout logique puissance 4

--- game/game.go
+++ game/game.go
@@ -17,6 +17,28 @@ func game() {
 		{3, 3, 3, 3, 3, 3, 3, 3, 3},
 	}
 
-	fmt.Println(grille[0][1])
+	//Yo les potes j'ai fait ça pour ajouter un jeton dans la premiere ligne de la grille mais je pense ça peut s'opti avec une boucle
+	choix_joueur := 0
+	fmt.Println("Choisis l'emplacement de ton jeton sur la première ligne 1 = tout en bas à gauche, 7 = tout en bas à droite")
+	fmt.Scan(&choix_joueur)
+	switch choix_joueur {
+	case 1:
+		grille[6][1] = (grille[6][1]) + 1
+	case 2:
+		grille[6][1] = (grille[6][1]) + 1
+	case 3:
+		grille[6][2] = (grille[6][1]) + 1
+	case 4:
+		grille[6][3] = (grille[6][1]) + 1
+	case 5:
+		grille[6][4] = (grille[6][1]) + 1
+	case 6:
+		grille[6][5] = (grille[6][1]) + 1
+	case 7:
+		grille[6][6] = (grille[6][1]) + 1
+	}
+	fmt.Println(grille)
+	//rajouter uine fonction pour savoir si la colonne est pleine
+	//et une fonction pour checker la win
 
 }
