49d86e5423dc7b57311010b2ed7f7b7ab0138000
Emrick <emrick.rivet@ynov.com>
2025-10-13 12:40:57 +0200

    Ajout et modif pour le début de partie

--- controller/controller.go
+++ controller/controller.go
@@ -6,6 +6,12 @@ import (
 	"power4/game"
 )
 
+// renderTemplate est une fonction utilitaire pour afficher un template HTML avec des données dynamiques
+func renderTemplate(w http.ResponseWriter, filename string, data map[string]string) {
+	tmpl := template.Must(template.ParseFiles("template/" + filename)) // Charge le fichier template depuis le dossier "template"
+	tmpl.Execute(w, data)                                              // Exécute le template et écrit le résultat dans la réponse HTTP
+}
+
 type PageData struct {
 	Title   string
 	Message string
@@ -30,21 +36,45 @@ func About(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, data)
 }
 
+// Contact gère la page de contact
 func Contact(w http.ResponseWriter, r *http.Request) {
-	data := PageData{
-		Title:   "Contact",
-		Message: "Envoie-nous un message 📩",
+	if r.Method == http.MethodPost { // Si le formulaire est soumis en POST
+		// Récupération des données du formulaire
+		name := r.FormValue("name") // Récupère le champ "name"
+		msg := r.FormValue("msg")   // Récupère le champ "msg"
+
+		data := map[string]string{
+			"Title":   "Contact",
+			"Message": "Merci " + name + " pour ton message : " + msg, // Message personnalisé après soumission
+		}
+		renderTemplate(w, "contact.html", data)
+		return // On termine ici pour ne pas exécuter la partie GET
 	}
-	tmpl := template.Must(template.ParseFiles("template/contact.html"))
-	tmpl.Execute(w, data)
+
+	// Si ce n'est pas un POST, on affiche simplement le formulaire
+	data := map[string]string{
+		"Title":   "Contact",
+		"Message": "Envoie-nous un message 📩",
+	}
+	renderTemplate(w, "contact.html", data)
 }
 
 func Jeu(w http.ResponseWriter, r *http.Request) {
-	grille := game.Game()
+	if r.Method == http.MethodPost {
+		grille := game.Game()
+		data := PageData{
+			Title:   "Jeu en cours",
+			Message: "C'est au tour de ",
+			Tableau: grille.Tableau,
+		}
+		tmpl := template.Must(template.ParseFiles("template/jeu.html"))
+		tmpl.Execute(w, data)
+		return
+	}
+
 	data := PageData{
 		Title:   "Entrée des joueurs",
 		Message: "Saisissez les noms des Joueurs",
-		Tableau: grille.Tableau,
 	}
 	tmpl := template.Must(template.ParseFiles("template/jeu.html"))
 	tmpl.Execute(w, data)
