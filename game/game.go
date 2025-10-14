773564528caa51d28b05bb672b33b7ceb99b2d53
Emrick <emrick.rivet@ynov.com>
2025-10-14 10:23:04 +0200

    Ajout de fonction tour joueur

--- game/game.go
+++ game/game.go
@@ -1,10 +1,16 @@
 package game
 
+import (
+	"log"
+	"net/http"
+	"strconv"
+)
+
 type Grille struct {
 	Tableau [8][9]int
 }
 
-func Game() *Grille {
+func InitGame() *Grille {
 
 	return &Grille{
 
@@ -26,36 +32,22 @@ func Game() *Grille {
 	}
 }
 
-//Yo les potes j'ai fait ça pour ajouter un jeton dans la premiere ligne de la grille mais je pense ça peut s'opti avec une boucle
-/*choix_joueur := 0
-	fmt.Println("Choisis l'emplacement de ton jeton sur la première ligne 1 = tout en bas à gauche, 7 = tout en bas à droite")
-	fmt.Scan(&choix_joueur)
-	switch choix_joueur {
-	case 1:
-		grille[6][1] = (grille[6][1]) + 1
-	case 2:
-		grille[6][1] = (grille[6][1]) + 1
-	case 3:
-		grille[6][2] = (grille[6][1]) + 1
-	case 4:
-		grille[6][3] = (grille[6][1]) + 1
-	case 5:
-		grille[6][4] = (grille[6][1]) + 1
-	case 6:
-		grille[6][5] = (grille[6][1]) + 1
-	case 7:
-		grille[6][6] = (grille[6][1]) + 1
+func Tour_joueur(grille *Grille, r *http.Request) {
+	colStr := r.FormValue("colonne")
+	col, err := strconv.Atoi(colStr)
+	if err != nil {
+		log.Println("Erreur conversion colonne :", err)
 	}
-	fmt.Println(grille)
-	for i := 0; i < len(grille); i++ {
-		for j := 0; j < len(grille[i]); j++ {
-			fmt.Print(grille[i][j], " ")
-		}
-		fmt.Println() // retourne à la ligne après chaque ligne de la matrice
-	}
-	//rajouter uine fonction pour savoir si la colonne est pleine
-	//et une fonction pour checker la win
 
+	for ligne := 6; ligne >= 1; ligne-- {
+		if grille.Tableau[ligne][col] == 0 {
+			grille.Tableau[ligne][col]++
+			/* Ajouter dans structure position dernier pion joué:
+			Position[0] == ligne
+			Position[1] == col */
+			break
+		}
+	}
 }
 
 /*func IsWin(){
