76c55234ddd49a71b0451beb557c85580d1b781d
Harld9 <harold.francois@ynov.com>
2025-10-16 12:20:50 +0200

    Modifi JSON

--- game/game.go
+++ game/game.go
@@ -6,6 +6,16 @@ import (
 	"strconv"
 )
 
+type JoueurStats struct {
+    Victoires     int `json:"victoires"`
+    Defaites      int `json:"defaites"`
+    PartiesJouees int `json:"parties_jouees"`
+}
+
+type Data struct{
+    Joueurs      map[string]JoueurStats `json:"joueurs"`
+    TotalParties int                    `json:"total_parties"`
+
 type Position struct {
 	Ligne int
 	Col   int
@@ -207,17 +217,4 @@ func DrawCheck(g *GameData) bool {
 	return true
 }
 
-/*func History(){
-
-
-
-
-
-
-}
-
-func Leaderboard() int{
-
-
-
-}*/
+func Leaderboard()
